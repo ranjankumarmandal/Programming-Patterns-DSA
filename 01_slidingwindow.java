039e7702c27bed8b3592a6ac16a93094f25e2672
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2025-04-19 08:11:43 +0530

    slidiingwindow: optimized approach O(n - k)

--- 01_slidingwindow.java
+++ 01_slidingwindow.java
@@ -1,3 +1,4 @@
+// =========================== find max sum of k-consecutive subarray elements ==== naive approach O(n - k) * O(k) ===================
 import java.util.*;
 
 public class Main {
@@ -21,6 +22,45 @@ public class Main {
             for(int j = i; j < i + k; j++) {
                 sum += arr[j];
             }
+            
+            maxSum = Math.max(maxSum, sum);
+        }
+        
+        return maxSum;
+    }
+}
+
+// ======================================================================== Optimized approach O(n - k) ==============================
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {7, 1, 8, 10, 12, 0, 5};
+        
+        // find max subarray sum of k elemnt
+        int k = 3;
+        
+        int maxSum = maxSumSubarrayWithWindowK(arr, k);
+        
+        System.out.println(maxSum);
+    }
+    
+    public static int maxSumSubarrayWithWindowK(int[] arr, int k) {
+        int maxSum = 0, n = arr.length;
+        
+        int sum = 0;
+        // find sum of 1st window 
+        for(int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+        
+        // minus last element and plus next element upto last index of array 
+        for(int i = 1; i <= n - k; i++) {
+            int prev = arr[i - 1];
+            int next = arr[i + k - 1];
+            
+            sum = sum - prev + next;
             
             maxSum = Math.max(maxSum, sum);
         }
